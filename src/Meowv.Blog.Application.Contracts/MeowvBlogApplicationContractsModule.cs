92b0ea20ffdb01971384c1a0a82b223a35a09098
帅帅一个伦 <49421274+zkl520cy@users.noreply.github.com>
2021-07-13 10:33:20 +0800

    Initialize v_3.4.0

---
+++ src/Meowv.Blog.Application.Contracts/MeowvBlogApplicationContractsModule.cs
@@ -0,0 +1,11 @@
+﻿using Meowv.Blog.Domain;
+using Volo.Abp.Modularity;
+
+namespace Meowv.Blog.Application.Contracts
+{
+    [DependsOn(typeof(MeowvBlogDomainModule))]
+    public class MeowvBlogApplicationContractsModule : AbpModule
+    {
+
+    }
+}
