55c1236366cf5db9c63739d90d3337244d5d1699
帅帅一个伦 <49421274+zkl520cy@users.noreply.github.com>
2021-07-13 10:33:16 +0800

    Initialize v_2.0.0

---
+++ Meowv.Web/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Meowv.Web.Controllers
+{
+    [Route("api")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
