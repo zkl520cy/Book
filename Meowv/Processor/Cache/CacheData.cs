8f29d2d34cad91e1679000a7b45009334bc21b98
帅帅一个伦 <49421274+zkl520cy@users.noreply.github.com>
2021-07-13 10:33:16 +0800

    Initialize v_1.0.0

---
+++ Meowv/Processor/Cache/CacheData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Meowv.Processor.Cache
+{
+    public class CacheData<T>
+    {
+        public DateTime ExpirationTime { get; set; }
+        public T Data { get; set; }
+    }
+}
